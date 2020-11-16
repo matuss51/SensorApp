cc378deab9e8e23f294d4687963aaf44ef4a7e0b
matuss51 <salajmatus@gmail.com>
2020-11-16 21:16:30 +0100

    kkk

---
+++ SensorApp/SensorApp/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+namespace SensorApp.Models
+{
+    public class Constants
+    {
+        public static bool isDev = true;
+
+        public static Color BackgroundColor = Color.FromRgb(58,153,215);
+        public static Color MainTextColor = Color.White;
+
+        public static int LoginIconHeight = 120;
+    }
+}
